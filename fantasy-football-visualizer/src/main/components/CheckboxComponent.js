39a3525d3056b8985ddc3b18d1c7cccc58895d49
Brendan Carr <brendan.c34@gmail.com>
2023-09-06 00:26:01 -0700

    FFV-26: format checkbox

--- fantasy-football-visualizer/src/main/components/CheckboxComponent.js
+++ fantasy-football-visualizer/src/main/components/CheckboxComponent.js
@@ -1,14 +1,23 @@
 import React from 'react';
 
 function CheckboxComponent({ showImage, onCheckboxChange }) {
+
+    const inputStyles = {
+        width: '18px',       // Set the desired width
+        height: '18px',      // Set the desired height
+        // fontSize: '16px',    // Set the desired font size
+        // padding: '8px',      // Add padding for better appearance
+        marginRight: '10px' 
+      };
+
   return (
     <div>
-      <label style={{padding:'100px'}}>
+      <label>
         <input
           type="checkbox"
           checked={showImage}
           onChange={onCheckboxChange}
-          style={{marginRight:'10px'}}
+          style={inputStyles}
         />
         Active Only
       </label>
